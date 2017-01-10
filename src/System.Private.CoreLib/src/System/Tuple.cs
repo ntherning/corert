e5d22b97b0d52c9ad3c7e9587d41f3b71172718c
Marek Safar <marek.safar@gmail.com>
2017-01-10 18:02:35 +0100

    [mono] Diagnostics in Tuple

--- src/System.Private.CoreLib/src/System/Tuple.cs
+++ src/System.Private.CoreLib/src/System/Tuple.cs
@@ -3,7 +3,7 @@
 // See the LICENSE file in the project root for more information.
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
+using System.Diagnostics.Private;
 using System.Runtime.CompilerServices;
 using System.Text;
 
